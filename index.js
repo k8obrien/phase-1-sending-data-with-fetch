974ca73dbc40fa4924aefcf9218349508a8c4c16
kateobrien <k8obrien2811@gmail.com>
2021-11-20 19:27:46 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email){
+    let fetchPost = fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify({
+            name:`${name}`,
+            email: `${email}`
+        })
+    })
+    .then(response => response.json())
+    .then(object => {
+        const li = document.createElement('li')
+        li.innerHTML = object.id
+        document.querySelector('body').appendChild(li)
+    })
+    .catch(error => {
+        const nli = document.createElement('li')
+        nli.innerHTML = error.message
+        document.querySelector('body').appendChild(nli)
+    })
+    return fetchPost
+}
